8ebd1847a13e9669895ee22d731a9663f7f657a5
nakashimn <nakashimn@gmail.com>
2024-11-24 22:39:44 +0900

    modify:update_codebuild_notification

--- terraform/modules/codebuild-notification-webhook-repo/variables.tf
+++ terraform/modules/codebuild-notification-webhook-repo/variables.tf
@@ -2,5 +2,6 @@
 # Params
 ################################################################################
 variable "image_tag" { description = "ECRリポジトリのイメージタグ" }
+variable "profile" { description = "利用するプロファイル名" }
 variable "region" { description = "利用するリージョン" }
 variable "repository_name" { description = "ECRリポジトリ名" }
